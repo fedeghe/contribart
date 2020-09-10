05af64b35833fb99f1e5c2f498226c249958ec7e
fedeghe <fedeghe@gmail.com>
2020-09-11 00:34:27 +0200

    simply works

--- write.sh
+++ write.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 echo 'Contribart ' > slave.txt
-date >> slave.txt 
-git add slave.txt
-git commit -m 'just write'
+
+for i in {1..20}
+do
+    date >> slave.txt 
+    git add slave.txt
+    git commit -m 'just write'
+done
+
 git push
